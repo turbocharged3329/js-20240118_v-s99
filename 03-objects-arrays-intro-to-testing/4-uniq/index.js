ed3bcc07df64c8a7c6e0cf450ee39ca88869a737
Vlad <vs3392rus@gmail.com>
2024-02-11 22:09:31 +0300

    задача 3.4

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,12 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  const resultArr = [];
+  const uniqValues = new Set(arr);
+    
+  for (let value of uniqValues) {
+    resultArr.push(value);
+  }
 
+  return resultArr;
 }
