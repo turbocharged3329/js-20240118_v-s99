7b653e7f082956bcc931b777cbf90a782054158a
Vlad <vs3392rus@gmail.com>
2024-02-11 21:44:47 +0300

    задача 3.3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (typeof size === 'undefined') {
+    return string;
+  }
 
+  if (!size) {
+    return '';
+  }
+
+  let stringSplitted = string.split('');
+  let counter = 0;
+  let repeatingSymbol = stringSplitted[0];
+  
+  for (let i = 1; i < stringSplitted.length; i++) {
+    if (stringSplitted[i] === repeatingSymbol) {
+      counter++;
+  
+      if (counter === size) {
+        stringSplitted.splice(i, 1);
+        return trimSymbols(stringSplitted.join(''), size);
+      }
+    } else {
+      repeatingSymbol = stringSplitted[i];
+      counter = 0;
+    }
+  }
+  
+  return stringSplitted.join('');
 }
